99de4b4e53d1c6c8c478e4c00a0ab1685ba17bb2
carlos merchan <77285543+CarlosMerchan@users.noreply.github.com>
2022-04-29 16:25:42 -0500

    Add files via upload
    documentacion y manejo de varios jugadores

--- src/modelo/Jugador.java
+++ src/modelo/Jugador.java
@@ -7,6 +7,7 @@ public class Jugador {
  
  private final int  DADO_MAX= 6;
  private int posicion = 0;
+ private int numeroJugador = 0;
  
  
  
@@ -16,6 +17,7 @@ public class Jugador {
 	 
  }
  
+ //Metodo que retorna un numero aleatorio entre 1 y 6
  public int tirarDado() {
 	 Random aleatoria = new Random();
 	 int numero = aleatoria.nextInt(DADO_MAX) + 1;
@@ -23,15 +25,17 @@ public class Jugador {
 	 return numero;
  }
  
- 
+ //obtiene la posicion de la variable privada posicion
  public int  getPosicion() {
 	 return posicion;
  }
  
+ //setea el valor de la posicion
  public void setPosicion(int posicion) {
 	 this.posicion = posicion;
  }
  
+ //obtiene el valor del dado y lo suma a la posicion actual del jugador
  public void avanzar(int jugada) {
 	 int valorDado = tirarDado();
 	 System.out.println(jugada+" Dado arroja " + valorDado);
@@ -39,7 +43,15 @@ public class Jugador {
 	 	
 		
 }
+
+ public void setNumeroJugador(int jugadores) {
+	 this.numeroJugador = jugadores;
+ }
  
+ 
+ public int getNumeroJugador() {
+	 return this.numeroJugador;
+ }
  
 
 }
--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -10,14 +10,16 @@ public class Tablero {
 	
 	
 	
-	
-	public Tablero(Jugador jugador) {
+	//metodo constructor que setea la instacia del jugador e inicia el juego, tambien indica el numero del jugador
+	public Tablero(Jugador jugador,int numeroJugador) {
 		this.jugador = jugador;
+		jugador.setNumeroJugador(numeroJugador);
 		iniciarJuego();
 	}
 	
+	//metodo que inicia el bucle del juego
 	private void iniciarJuego() {
-		 
+		 System.out.println("Nuevo Jugador numero de jugador "+ jugador.getNumeroJugador());
 		 while(play) {
 			 jugador.avanzar(jugada);
 			 jugada++;
@@ -26,62 +28,65 @@ public class Tablero {
 			 
 			 }
 	 }
-	
+	 
+	/*metodo que valida a traves de la posicion del jugador si termino en una escalera o una serpiente y 
+	 * lo mueve a la casilla correspondiente
+	 */
 	private void validarPosicion(int posicion) {
 		
 		switch(posicion) {
 		
 		case 3 :
 			jugador.setPosicion(11);
-			System.out.println(jugada+" Jugador sube por escalera  a:" + jugador.getPosicion());
+			System.out.println(jugada+" Jugador N."+jugador.getNumeroJugador()+" sube por escalera  a:" + jugador.getPosicion());
 			 jugada++;
 			break;
 			
 		case 6:
 			jugador.setPosicion(17);
-			System.out.println(jugada+" Jugador sube por escalera  a:" + jugador.getPosicion());
+			System.out.println(jugada+" Jugador N."+jugador.getNumeroJugador()+" sube por escalera  a:" + jugador.getPosicion());
 			 jugada++;
 			break;
 			
 		case 9:
 			jugador.setPosicion(18);
-			System.out.println(jugada+" Jugador sube por escalera  a:" + jugador.getPosicion());
+			System.out.println(jugada+" Jugador N."+jugador.getNumeroJugador()+" sube por escalera  a:" + jugador.getPosicion());
 			 jugada++;
 				break;
 				
 		case 10:
 			jugador.setPosicion(12);
-			System.out.println(jugada+" Jugador sube por escalera  a:" + jugador.getPosicion());
+			System.out.println(jugada+" Jugador N."+jugador.getNumeroJugador()+" sube por escalera  a:" + jugador.getPosicion());
 			 jugada++;
 			break;	
 	
 		case 14:
 			jugador.setPosicion(4);
-			System.out.println(jugada+" Jugador deciende   a:" + jugador.getPosicion());
+			System.out.println(jugada+" Jugador N."+jugador.getNumeroJugador()+" deciende   a:" + jugador.getPosicion());
 			 jugada++;
 			break;
 			
 		case 19:
 			jugador.setPosicion(8);
-			System.out.println(jugada+" Jugador desciende a:" + jugador.getPosicion());
+			System.out.println(jugada+" Jugador N."+jugador.getNumeroJugador()+" desciende a:" + jugador.getPosicion());
 			 jugada++;
 			break;
 			
 		case 22:
 			jugador.setPosicion(20);
-			System.out.println(jugada+" Jugador desciende a:" + jugador.getPosicion());
+			System.out.println(jugada+" Jugador N."+jugador.getNumeroJugador()+" desciende a:" + jugador.getPosicion());
 			 jugada++;
 				break;
 				
 		case 24:
 			jugador.setPosicion(16);
-			System.out.println(jugada+" Jugador desciende a:" + jugador.getPosicion());
+			System.out.println(jugada+" Jugador N."+jugador.getNumeroJugador()+" desciende a:" + jugador.getPosicion());
 			 jugada++;
 			
 			break;
 			
 		default :
-			System.out.println(jugada+" Jugador avanza a cuadro :" + jugador.getPosicion());
+			System.out.println(jugada+" Jugador N."+jugador.getNumeroJugador()+" avanza a cuadro :" + jugador.getPosicion());
 			 jugada++;
 
 			break;
@@ -90,12 +95,22 @@ public class Tablero {
 		
 	}
 	
+	/*metodo para validar deternimar si ya llego a la casilla 25 para acabar el loop
+	 * sin no, valida si callo sea en serpiente o escalera y mueve al jugador por el tablero
+	 */
 	public void validarFin(int posicion) {
-		if(posicion >= MAX_TABLERO){
-			System.out.println(jugada+ " El jugador llego o supero el cuadro "+MAX_TABLERO);
-			System.out.println((jugada+1)+ " Fin" );
+		if(posicion == MAX_TABLERO){
+			System.out.println(jugada+ " El jugador N."+ jugador.getNumeroJugador()+" llego cuadro "+MAX_TABLERO);
+			System.out.println((jugada+1)+ "Jugador N."+jugador.getNumeroJugador() +" Fin" );
 			play = false;
-		}else {
+		}else if(posicion > MAX_TABLERO){
+			int valorRetroceso = posicion - MAX_TABLERO;
+			jugador.setPosicion(MAX_TABLERO - valorRetroceso);
+			
+			System.out.println(jugada + " El jugador N."+jugador.getNumeroJugador()+" retrocedio "+valorRetroceso+" ya que supero la casilla 25");
+			System.out.println("Posicion Actual jugador N."+jugador.getNumeroJugador()+" es =" + jugador.getPosicion());
+			jugada++;
+		}else{
 			validarPosicion(jugador.getPosicion());
 		}
 	}
