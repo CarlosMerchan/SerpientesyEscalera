11ab5e8d94e943ff31854ec7c80a432af540cea8
carlos merchan <77285543+CarlosMerchan@users.noreply.github.com>
2022-04-29 16:24:52 -0500

    Add files via upload
    se añadio numero de jugadores

--- src/serpientesyEscaleras/EscalerasySerpiente.java
+++ src/serpientesyEscaleras/EscalerasySerpiente.java
@@ -1,13 +1,32 @@
 package serpientesyEscaleras;
 
+import java.util.Scanner;
+
 import modelo.Jugador;
 import modelo.Tablero;
 
 public class EscalerasySerpiente {
 
 	public static void main(String[] args) {
-		Jugador jugador = new Jugador();
-		Tablero tablero = new Tablero(jugador);
+		
+		boolean pregunta = true;
+		int cantidadJugadores = 0;
+		Scanner scan = new Scanner(System.in);
+		while(pregunta) {
+			System.out.println("Por favor Indicar numero de jugadores:");
+			 cantidadJugadores = scan.nextInt();
+			if(cantidadJugadores > 0) {
+				pregunta = false;
+			}
+		}
+		
+		for(int i = 0; i < cantidadJugadores;i++){
+			Jugador jugador = new Jugador();
+			Tablero tablero = new Tablero(jugador,i+1);
+		
+		}
+		scan.close();
+		
 
 	}
 
